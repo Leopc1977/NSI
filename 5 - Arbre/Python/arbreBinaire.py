e688e93889a89af589b6e05ffe43e0dc327eae56
Leopc1977 <leophancaowz@gmail.com>
2021-12-08 18:29:25 +0100

    no

--- 5 - Arbre/Python/arbreBinaire.py
+++ 5 - Arbre/Python/arbreBinaire.py
@@ -17,16 +17,90 @@ class Node:
         print(res)
     def getSize(s,node,size):
         if not node.left and not node.right: #is a leaf
-            return size
+            return 1
         sizeLeft,sizeRight = 0,0
         if node.left:
             sizeLeft = s.getSize(node.left,size)
         if node.right:
             sizeRight = s.getSize(node.right,size)
         return size+sizeLeft+sizeRight
+    def getTree(s,node,lstTree): #to finish
+        if not node.left and not node.right: #is a leaf
+            return node.key
 
-a = Node(3)
-b = Node(2,a)
-c = Node(0)
-d = Node(1,c,b)
-print(d.getSize(d,1))
+        currentBranch = [node.key]
+        branchsToAppend = [] 
+
+        if node.left:
+            treeLeft = s.getTree(node.left,lstTree)
+            branchsToAppend.append(treeLeft)
+        if node.right:
+            treeRight = s.getTree(node.right, lstTree)
+            branchsToAppend.append(treeRight)
+        else: 
+            branchsToAppend.append(None)
+        currentBranch.append(branchsToAppend)
+
+        if s !=node:
+            lstTree.append(currentBranch)
+            
+        return lstTree
+    def isLeaf(s):
+        return not s.left and not s.right
+    def isInTree(s,keySearched):
+        print(s.key)
+
+        if s.key == keySearched:
+            return True
+        if s.isLeaf():
+            return False
+    
+        left,right=False,False
+        if s.left:
+            left = s.left.isInTree(keySearched)
+        if s.right:
+            right = s.right.isInTree(keySearched)
+        if left==True or right==True:   
+            return True
+    def getLeaves(s,lstLeaves):
+        if s.isLeaf():
+            return lstLeaves.append(s.key)
+        else:
+            left,right=[],[]
+            if s.left:
+                left = s.left.getLeaves(lstLeaves)
+            if s.right:
+                right = s.right.getLeaves(lstLeaves)
+            if type(left)==list:
+                if len(left)>0:
+                    lstLeaves.append(left)
+            if type(right)==list:
+                if len(right)>0:
+                    lstLeaves.append(right)
+        return lstLeaves        
+def createBT(lstOfKeys):
+    lstNode = [Node() for i in range(len(lstOfKeys)//2)]
+    for k in range(len(lstOfKeys)):
+        try:
+            lstNode[k].key = lstOfKeys[k]
+            if lstOfKeys[2*k+1]:
+                lstNode[k].left = Node(lstOfKeys[k*2+1])
+            if lstOfKeys[2*k+2]:
+                lstNode[k].right = Node(lstOfKeys[k*2+2])
+        except:
+            pass
+    node = lstNode[0]
+    for n in lstNode :
+        n.print()
+    return node
+
+a = Node(4,Node(5),Node(6))
+b = Node(3,Node(8))
+e = Node(2,a,b)
+f = Node (1,e)
+
+tree = f.getTree(f,[f.key])
+leaves = f.getLeaves([])
+
+lst = [0,1,2,3,4,5,6]
+node = createBT(lst)
