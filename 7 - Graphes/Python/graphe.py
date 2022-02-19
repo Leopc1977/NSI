787100b93b17a98cdfb23791f85aa560179bf4e0
Leopc1977 <leophancaowz@gmail.com>
2022-02-19 17:42:37 +0100

    yes

--- 7 - Graphes/Python/graphe.py
+++ 7 - Graphes/Python/graphe.py
@@ -1,5 +1,6 @@
+from math import dist
 from random import randint, shuffle
-
+import turtle
 def dictToAdjMatrix(dict):
     n = len(dict)
     res = [[0 for _ in range(n)] for _ in range(n)]
@@ -8,7 +9,7 @@ def dictToAdjMatrix(dict):
         for j in dict[i]:
             res[i][j]=1
     return res
-arbre = { 0:[1 , 2 , 3] , 1:[4 , 5 ] , 2:[6] , 3:[7 , 8] , 4:[] , 5:[] , 6:[] , 7:[] , 8 : [9,10] , 9:[], 10:[]}
+arbre = { 0:[1 , 2 ] , 1:[4 , 5 ] , 2:[6] , 3:[7 , 8] , 4:[] , 5:[] , 6:[] , 7:[] , 8 : []}
 AdjMatrix = dictToAdjMatrix(arbre)
 
 def adjMatrixToDict(lst):
@@ -39,4 +40,38 @@ def isOriented(adjMatrice):
             if adjMatrice[i][j]!=adjMatrice[j][i]:
                 return False
     return True
+
 dictTest = createDict(5)
+
+from turtle import *
+speed("fastest")
+
+AdjMatrix = dictToAdjMatrix(createDict(1000))
+r = 200
+n=len(AdjMatrix)
+up()
+lst_pos=[]
+for i in range(n):
+    forward(r)
+    #circle(10)
+    write(i)
+    lst_pos.append(position())
+    backward(r)
+    left(360/n)
+
+for i in range(len(AdjMatrix)):
+    startX ,startY = lst_pos[i][0],lst_pos[i][1]
+    goto(startX,startY)
+    for j in range(len(AdjMatrix[i])):
+        if AdjMatrix[i][j]==1:
+            down()
+            destX,destY=lst_pos[j][0],lst_pos[j][1]
+            """towards(destX,destY)
+            moitieDist = dist([startX,startY],[destX,destY])*2/3
+            setheading(towards(destX,destY))
+            forward(moitieDist)
+            stamp()"""
+            goto(destX,destY)
+            up()
+
+turtle.Screen().exitonclick()
